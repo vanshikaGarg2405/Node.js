581c4776b342118cb33f371d8d67e0e710800b14
vanshikaGarg2405 <gargm4932@gmail.com>
2026-08-19 10:27:42 +0530

    Buffer & Stream

--- FileModule/Stream.js
+++ FileModule/Stream.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const readStream = fs.createReadStream("./test.txt");
+const writeStream = fs.createWriteStream('./output.txt');
 readStream.on('data', (chunk) => {
     console.log(chunk);
     console.log(chunk.toString());
+    writeStream.write(chunk);
 })
 readStream.on('end', () => {
     console.log("File reading completed.");
 })
-
-const writeStream = fs.createWriteStream('./output.txt');
-readStream.pipe(writeStream);
