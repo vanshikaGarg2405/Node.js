688dd1708324ac95a0fe8234d7c67ad12a21f77d
vanshikaGarg2405 <gargm4932@gmail.com>
2026-08-25 11:13:24 +0530

    Server

--- HTTP/server.js
+++ HTTP/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+//const JSON = 
 // const server = http.createServer((req, res) => {
 //     res.end("Hello")
 // });
@@ -9,18 +11,45 @@ const http = require("http");
 
 const PORT = 4000;
 const server = http.createServer((req, res) => {
-    if(req.url === "/" && req.method === "GET") {
-        res.end("You are at home page.");
-    }
-    else if(req.url === "/About" && req.method === "GET") {
-        res.end("You are at about page.");
-    }
-    else if(req.url === "/Contact" && req.method === "GET") {
-        res.end("Contact us");
-    }
-    else {
-        res.statusCode = 404;
-        res.end("Page not found");
+    // if(req.url === "/" && req.method === "GET") {
+    //     //console.log(req.headers);
+    //     res.end("You are at home page.");
+    // }
+    // else if(req.url === "/About" && req.method === "GET") {
+    //     res.end("You are at about page.");
+    // }
+    // else if(req.url === "/Contact" && req.method === "GET") {
+    //     res.end("Contact us");
+    // }
+    // else {
+    //     res.statusCode = 404;
+    //     res.end("Page not found");
+    // }
+    // switch(req.url) {
+    //     case "/" : {
+    //         res.end("You are at home page.");
+    //         break;
+    //     }
+    //     case "/About" :  {
+    //         res.end("You are at about page.");
+    //         break;
+    //     }
+    //     case "Conatct" : {
+    //         res.end("Contact us");
+    //         break;
+    //     }
+    //     default : {
+    //         res.statusCode = 404;
+    //         res.end("Page not found");
+    //     }
+    // }
+    if(req.url == "/") {
+        const User = {
+            name: "Vanshika",
+            branch: "CSE",
+            roll: "027"
+        };
+        res.end(JSON.stringify(User));
     }
 });
 server.listen(PORT, () => {
