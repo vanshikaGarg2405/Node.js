b5a5e7957f2b7203b17dede734e73bd2cde27dfa
vanshikaGarg2405 <gargm4932@gmail.com>
2026-09-07 10:44:02 +0530

    Express

--- Express/trial.js
+++ Express/trial.js
@@ -1,11 +1,74 @@
 import express from 'express'
 const app = express();
+const PORT = 8000;
 
-// Runs for every single incoming request
-app.use((req, res, next) => {
-    console.log("Moddleware Executed!");
-    next(); // Moves to the next middleware or route handler
+//Application Level Middleware
+//Not-mount on path
+// app.use((req, res, next) => {
+//     console.log("Middleware 1");
+//     next();
+// })
+
+// app.use((req, res, next) => {
+//     console.log("Middleware 2");
+//     next();
+// })
+
+//Mount on path
+// app.use('/student/:id', (req, res, next) => {
+//     console.log("Response Type", req.method)
+//     next();
+// })
+
+// app.get('/student', (req, res) => {
+//     console.log("Router Middleware");
+//     res.send("Home Page")
+// })
+
+//Multiple Route Handler
+// app.use('/user/:id', 
+//     (req, res, next) => {
+//         console.log("Requested URL: ", req.url)
+//         next();
+//     },
+//     (req, res, next) =>{
+//         console.log("Request Type: ", req.method)
+//         next();
+//     }
+// )
+
+// app.use('/students/:id', 
+//     (req, res, next) => {
+//         const id = parseInt(req.params.id);
+//         if(id === 0) next('route');
+//         else {
+//             console.log(req.method);
+//             next();
+//         }
+//     },
+//     (req, res, next) => {
+//         res.send("Routing handeled");
+//     }
+// )
+// app.use('/students/:id', (req, res) => {
+//     res.send("Routing handled if id = 0");
+// })
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res.status().send("Error Handeled");
+// })
+
+
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.post('/user', (req, res) => {
+    console.log(req.body); 
+    res.send({ status: "Success", dataReceived: req.body });
 });
-app.get('/', (req, res) => {
-    
+
+app.listen(PORT, () => {
+    console.log("Server is listening...");
 })
